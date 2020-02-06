553d0005acecc8dcb215a45915d6dcffb9c7836a
Matheus Novais <mths_campos@hotmail.com>
2020-02-06 16:11:01 -0300

    Recurso estático implementado

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ const app = express();
 
 app.get("/", (req, res) => res.json({status: "NTask API"}));
 
+app.get("/tasks", (req, res) => {
+     res.json({ 
+         tasks: [ 
+             {title: "Fazer compras"}, 
+             {title: "Consertar o pc"},
+         ] 
+    }); 
+});
+
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
+
