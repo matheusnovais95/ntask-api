e57b0c6da927e58fff755fb8ba38fadbddd3fb69
Matheus Novais <mths_campos@hotmail.com>
2020-02-06 16:50:06 -0300

    criação rotas, dos models e das libs

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import express from "express"; 
-const PORT = 3000;
+import consign from "consign";
+
 
 const app = express();
 
-app.get("/", (req, res) => res.json({status: "NTask API"}));
+consign() 
+    .include("models")
+    .then("libs/middlewares.js")
+    .then("routes")
+    .then("libs/boot.js")
+    .into(app);
 
-app.get("/tasks", (req, res) => {
-     res.json({ 
-         tasks: [ 
-             {title: "Fazer compras"}, 
-             {title: "Consertar o pc"},
-         ] 
-    }); 
-});
 
-app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
 
